7805e3fc1d465d5dcdeb284539ae998df2f33b3d
Richyms <mora.santillan.ricardo@gmail.com>
2021-11-05 09:07:23 -0600

    primera version

---
+++ app/src/main/java/com/ricardo/mora/santillan/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ricardo.mora.santillan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
