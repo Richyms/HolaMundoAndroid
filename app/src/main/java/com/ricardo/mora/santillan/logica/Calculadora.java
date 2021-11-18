082df4628f44b0525811a245aa5dda287d3e3887
Richyms <mora.santillan.ricardo@gmail.com>
2021-11-17 19:41:49 -0600

    Resta

---
+++ app/src/main/java/com/ricardo/mora/santillan/logica/Calculadora.java
@@ -0,0 +1,23 @@
+package com.ricardo.mora.santillan.logica;
+
+import java.io.Serializable;
+
+public class Calculadora implements Serializable
+{
+    public void suma( )
+    {
+
+    }
+    public void resta( )
+    {
+
+    }
+    public void multiplicacion( )
+    {
+
+    }
+    public void division( )
+    {
+
+    }
+}
